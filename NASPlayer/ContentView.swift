b7194c88a1b50f5a1ae1643744889263e4f6dd60
Jon <ruisong20@gmail.com>
2023-09-19 14:45:43 +0800

    Initial Commit

---
+++ NASPlayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NASPlayer
+//
+//  Created by 宋睿 on 19/9/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NASPlayer/NASPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NASPlayerApp.swift
+//  NASPlayer
+//
+//  Created by 宋睿 on 19/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct NASPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
